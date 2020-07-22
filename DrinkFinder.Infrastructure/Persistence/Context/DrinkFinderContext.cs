79865b3bce47bd53e9b1f27e18d56f2a4e5cdb1a
Jinai <jinai.git@gmail.com>
2020-07-22 14:14:38 +0200

    Automatically update timestamps when saving changes to the context

--- DrinkFinder.Infrastructure/Persistence/Context/DrinkFinderContext.cs
+++ DrinkFinder.Infrastructure/Persistence/Context/DrinkFinderContext.cs
@@ -1,10 +1,13 @@
-﻿using DrinkFinder.Infrastructure.Persistence.Entities;
+﻿using DrinkFinder.Infrastructure.Persistence.Abstract;
+using DrinkFinder.Infrastructure.Persistence.Entities;
 using DrinkFinder.Infrastructure.Persistence.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DrinkFinder.Infrastructure.Persistence.Context
 {
@@ -12,6 +15,24 @@ namespace DrinkFinder.Infrastructure.Persistence.Context
     {
         public DrinkFinderContext(DbContextOptions<DrinkFinderContext> options) : base(options)
         {
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedAt = DateTime.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastEditedAt = DateTime.Now;
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
